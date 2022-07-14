346ec94b4f55a6660c1d215a371ddee4f226e333
awb99 <andreas.wolfgang.bauer@gmail.com>
2022-07-14 03:10:06 +0200

    stop

--- nginx-proxy/run.sh
+++ nginx-proxy/run.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# host-port:docker-port
+
 docker run -d \
     --name=nginx-proxy-manager \
     -p 8181:8181 \
-    -p 8080:8080 \
-    -p 4443:4443 \
+    -p 80:8080 \
+    -p 443:4443 \
     -v /home/florian/repo/crb/docker-proxy:/config:rw \
     jlesage/nginx-proxy-manager
---
+++ nginx-proxy/stop.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+docker stop nginx-proxy-manager
+
+# docker rm nginx-proxy-manager
